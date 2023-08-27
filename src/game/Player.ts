1fde78f9ab7c15db74607e8bb41ca2978c3577df
Beetroot Paul <paul@beetrootpaul.com>
2023-08-28 00:42:43 +0200

    rewrite from Lua: part 19

--- src/game/Player.ts
+++ src/game/Player.ts
@@ -1,4 +1,4 @@
-import { v_, Vector2d } from "@beetpx/beetpx";
+import { BeetPx, v_, Vector2d } from "@beetpx/beetpx";
 import { g } from "../globals";
 import { AnimatedSprite } from "../misc/AnimatedSprite";
 
@@ -8,7 +8,7 @@ export class Player {
   private readonly _shipSpriteFlyingRight: AnimatedSprite;
   private _shipSpriteCurrent: AnimatedSprite;
 
-  private readonly _xy: Vector2d;
+  private _xy: Vector2d;
 
   constructor() {
     // TODO
@@ -80,12 +80,17 @@ export class Player {
       : this._shipSpriteNeutral;
 
     // TODO
+    const speed = 60;
     //     local speed = fast_movement and 1.5 or 1
+    // TODO
+    const xDiff = right ? speed : left ? -speed : 0;
     //     local x_diff, y_diff = (right and speed or (left and -speed or 0)), (down and speed or (up and -speed or 0))
     //     if x_diff ~= 0 and y_diff ~= 0 then
     //         -- speed fix for a diagonal movement
     //         x_diff, y_diff = x_diff / 1.41, y_diff / 1.41
     //     end
+    // TODO
+    this._xy = this._xy.add(v_(xDiff, 0).mul(BeetPx.dt));
     //     xy = _xy(
     //         mid(w / 2 + 1, xy.x + x_diff, _gaw - w / 2 - 1),
     //         mid(h / 2 + 1, xy.y + y_diff, _gah - h / 2 - 1)
